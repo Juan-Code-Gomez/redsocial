a097fe4221651087845a29f35ae0d27dabe614f5
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2021-08-18 09:45:14 -0500

    Se agrega los usuarios y se validan en la base de datos

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 const express= require("express");
+const cors = require("cors");
 const {dbConnection} = require("./db/db");
+const User = require("./routers/user");
 require("dotenv").config();
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/user", User);
 
 app.listen(process.env.PORT,()=>
 console.log("Backend server running OK, on port:", process.env.PORT)
